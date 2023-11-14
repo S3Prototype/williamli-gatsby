1f88b4d1a172b2e6fb5e384392d732b8bb9d93f9
William Li <email@williamli.dev>
2023-11-15 00:32:49 +0800

    dynamic API route

--- src/api/blog/[id].js
+++ src/api/blog/[id].js
@@ -1,5 +1,8 @@
 export default function handler(request, response) {
+  const blogId = request.params.id;
+
   response.status(200).json({
+    blogId,
     body: request.body,
     query: request.query,
     cookies: request.cookies,
