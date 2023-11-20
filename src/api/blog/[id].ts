6b4c1b3aa4d4e921a88eaea51e15122c963e6b7e
S3Prototype <liuqahs15@gmail.com>
2023-11-20 11:31:42 -0600

    Initial commit

---
+++ src/api/blog/[id].ts
@@ -0,0 +1,14 @@
+import type { VercelRequest, VercelResponse } from '@vercel/node';
+
+export default function handler(request: VercelRequest & { params: { id: string}}, response: VercelResponse) {
+  // const blogId = request.params.id;
+
+  console.log(`/api/blog/${request.params?.id} in TS`);
+
+  response.status(200).json({
+    body: request.body,
+    params: `Params in TS: ${request.params}`,
+    query: `Query in TS: ${request.query}`,
+    cookies: request.cookies,
+  });
+}
--- src/api/products/[...].js
+++ src/api/products/[...].js
@@ -1,10 +1,11 @@
 export default function handler(request, response) {
   // const blogId = request.params.id;
 
-  console.log('/api/[id]');
+  console.log("HELLO THERE!!!!!!!!!!!!!!!!!", request.query);
 
   response.status(200).json({
     body: request.body,
+    params: request.params,
     query: request.query,
     cookies: request.cookies,
   });
